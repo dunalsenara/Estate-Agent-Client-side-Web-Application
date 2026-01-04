be9fb8bbcf49a97da333b6d5e52414f5fb4686a6
Dunal Senara <senaradunal@gnail.com>
2026-01-04 16:58:13 +0530

    change a part of location

--- src/App.jsx
+++ src/App.jsx
@@ -299,7 +299,6 @@ const PropertySearchApp = () => {
                         <iframe
                           width="100%"
                           height="100%"
-                          frameBorder="0"
                           style={{ border: 0 }}
                           src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8&q=${encodeURIComponent(selectedProperty.locationpath)}`}
                           allowFullScreen
